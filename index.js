232216393ad5e7e8d0b8f7dc736778f1ab4c00b4
westwoodelliot <wese1_12@uni.worc.ac.uk>
2015-05-02 21:31:18 +0100

    Added jquery
    Some pages have jquery, there are files changed and added to stop data
    charges. Issue with pagecreate and mapload

--- index.js
+++ index.js
@@ -92,3 +92,36 @@ function failPosition(error) {
 	$('#time').val("Error getting data: " + error);
 	
 }
+
+$( document ).on( "pagecreate", "#map-page", function() {
+    var defaultLatLng = new google.maps.LatLng(52.19812, -2.24185);
+    if ( navigator.geolocation ) {
+        function success(pos) {
+            // Location found, show map with these coordinates
+            drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        }
+        function fail(error) {
+            drawMap(defaultLatLng);  // Failed to find location, show default map
+        }
+        // Find the users current position.  Cache the location for 5 minutes, timeout after 6 seconds
+        navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 500000, enableHighAccuracy:true, timeout: 6000});
+    } else {
+        drawMap(defaultLatLng);  // No geolocation support, show default map
+    }
+    function drawMap(latlng) {
+        var myOptions = {
+            zoom: 16,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+        // Add an overlay to the map of current lat/lng
+        var marker = new google.maps.Marker({
+            position: latlng,
+            map: map,
+            draggable: true,
+		title:"Your Position!"
+        });
+		
+    }
+});
