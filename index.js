39089d6229d1aa917232a6acbcfefc42da231b43
westwoodelliot <wese1_12@uni.worc.ac.uk>
2015-05-03 16:41:44 +0100

    Changed options
    Each option shows map, unfortunately its the same map due to how map is
    loaded

--- index.js
+++ index.js
@@ -1,99 +1,4 @@
-//when the jQuery Mobile page is initialised
-/*$(document).on('pagecreate', function() {
-	
-	//set up listener for button click
-	//$(document).ready(getPosition);
-	
-	//change time box to show message
-	$('#time').val("Press the button to get location data");
-	
-});
-
-
-//Call this function when you want to get the current position
-function getPosition() {
-	
-	//change time box to show updated message
-	$('#time').val("Getting data...");
-	
-	//instruct location service to get position with appropriate callbacks
-	navigator.geolocation.getCurrentPosition(successPosition, failPosition);
-	
-	
-}
-
-//called when the position is successfully determined
-function successPosition(position) {
-	
-	
-	//lets get some stuff out of the position object
-	var unixTime = new Date(position.timestamp);
-	var date = unixTime.toTimeString();
-	var time = position.timestamp;
-	var latitude = position.coords.latitude;
-	var longitude = position.coords.longitude;
-	
-	//OK. Now we want to update the display with the correct values
-	$('#time').val("Received data at " + date);
-	$('#lattext').val("Your latitude is " + latitude);
-	$('#longtext').val("Your longitude is " + longitude);
-	if ('null' != latitude)
-	{
-	loadMap(position);
-	}
-	
-}
-function loadMap(position)
-{
-		
-		var latitude = position.coords.latitude;
-		var longitude = position.coords.longitude;
-		var myLatlng = new google.maps.LatLng(latitude,longitude);
-        var mapOptions = {
-          center: { lat: latitude, lng: longitude},
-          zoom: 16
-        };
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-			
-      var marker = new google.maps.Marker({
-		position: myLatlng,
-		map: map,
-		draggable: true,
-		title:"Your Position!"});
-		
-		 var drawingManager = new google.maps.drawing.DrawingManager({
-    drawingMode: google.maps.drawing.OverlayType.MARKER,
-    drawingControl: true,
-    drawingControlOptions: {
-      position: google.maps.ControlPosition.TOP_CENTER,
-      drawingModes: [
-        google.maps.drawing.OverlayType.MARKER,
-        google.maps.drawing.OverlayType.POLYLINE
-      ]
-    },
-    markerOptions: {
-      icon: 'images/filled_flag.png',
-	  draggable: true,
-    },
-    
-  });
-    drawingManager.setMap(map);
-	loadRoutes(map);
-
-		
-      google.maps.event.addDomListener(window, 'load', initialize);
-	  
-	  
-}
-//called if the position is not obtained correctly
-function failPosition(error) {
-	//change time box to show updated message
-	$('#time').val("Error getting data: " + error);
-	
-}
-*/
-$( document ).on( "pagecreate", "#map-page", function() {
+$( document ).on( "pageinit", "#map-page", function() {
     var defaultLatLng = new google.maps.LatLng(52.19812, -2.24185);
     if ( navigator.geolocation ) {
         function success(pos) {
@@ -122,6 +27,26 @@ $( document ).on( "pagecreate", "#map-page", function() {
             draggable: true,
 		title:"Your Position!"
         });
+		var drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.MARKER,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: [
+        google.maps.drawing.OverlayType.MARKER,
+        google.maps.drawing.OverlayType.POLYLINE
+      ]
+    },
+    markerOptions: {
+      icon: 'images/filled_flag.png',
+	  draggable: true,
+    },
+    polylineOptions: {
+		strokeColor: '#ff0000',
+		strokeWeight: 3
+	}
+  });
+  drawingManager.setMap(map);
 		
     }
 });
